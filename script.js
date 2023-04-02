4d379cee0da25b9fd18c3e87b34496a5489d2afc
Kess220 <prodvictorkaio@gmail.com>
2023-04-01 23:48:53 -0300

    js quase pronto, eu acho kk

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function verificar() {
+  const verification = document.querySelectorAll(".escolhido").length;
+  const footer = document.querySelector("footer");
+
+  if (verification === 3) {
+    footer.firstElementChild.classList.add("escolhido");
+    footer.firstElementChild.classList.remove("escolhido");
+  }
+}
 function selectionPrato(prato) {
   let pratoSelection = document.querySelector(".prato .escolhido");
 
@@ -15,5 +24,16 @@ function selectionBebida(bebida) {
   }
 
   bebidaSelection.classList.add("escolhido");
+  verificar();
+}
+
+function selectionSobremesa(sobremesa) {
+  let sobremesaSelection = document.querySelector(".sobremesa .escolhido");
+
+  if (!!sobremesaSelection) {
+    sobremesaSelection.classList.remove(".escolhido");
+  }
+
+  sobremesa.classList.add(".escolhido");
   verificar();
 }
